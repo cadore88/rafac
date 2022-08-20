6d54e5f5be6cc90584a170197c19ef9b77e5a3d0
rafael <cadorefloripa@gmail.com>
2022-08-19 22:46:23 -0300

    Sistema de eventos

---
+++ SistemaEventos.js
@@ -0,0 +1,23 @@
+
+
+let dataAtual = new Date(2022-8-15);
+let dataEvento =  Date(2022-9-10);
+let listaParticipantes = ["Marcos", "Juliana", "Bianca", "Rafael", "Tião"];
+let idadeParticipante = 15
+
+if (dataEvento>dataAtual) {
+    console.log("Agendar evento");
+    } else {console.log("Data Inválida");
+}
+console.log("------------------");
+
+if (idadeParticipante => 18) {
+    console.log("Entrada Permitida");
+    } else {console.log("Idade inválida")}
+
+
+// parei em 2:28 min
+
+
+
+
